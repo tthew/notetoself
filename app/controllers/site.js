c149f906aad2732ed1029a5aa0d700cfa9dd253f
Matt Richards <matt@lucidmoon.co.uk>
2013-01-14 18:22:29 +0000

    install express

--- app/controllers/site.js
+++
@@ -1,9 +0,0 @@
-/* put controller actions here */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
-};
-
-exports.detail = function(req, res){
-    res.render('detail', {title: 'Detail'});
-}
